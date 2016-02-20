141c57a2b5faad501a171e50236745228c534bd6
Shawn McElroy <shawn@skift.io>
2016-02-19 20:21:35 -0800

    trying to get this working, sharing code.

--- falcon/app/api.py
+++ falcon/app/api.py
@@ -1,12 +1,13 @@
 import hug
 
-from .resources import part_1, part_2
+from .resources import part_1
 
 
 @hug.get('/')
 def say_hi():
     return "Hi from root"
 
+
 @hug.extend_api()
 def with_other_apis():
-    return [part_1, part_2]
+    return [part_1]
